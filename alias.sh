08f5dce05da5a426a1de31acd572167f923fc644
Joonho Park <hopefulp@gmail.com>
2018-09-18 12:52:56 +0900

    more in chem

---
+++ alias.sh
@@ -0,0 +1,74 @@
+# directory hopping
+# home directory
+
+repo=/Data/Repository
+repo1=/Data/Repository/tf_skim_gs
+repo2=/Data/Repository/mc_mof_gs
+Res=$HOME/Research
+
+alias gores="cd $Res"
+alias goai="cd $Res/AI"
+alias goaig="cd $Res/AI/tf_skim_gl"
+alias gomofg="cd $Res/mof_mc_gl"
+alias gomofp="cd $Res/Mofplus"
+alias gorep="cd $repo"
+alias gos="cd $HOME/scripts"
+alias gobin="cd $HOME/bin"
+alias gobinv="cd $HOME/binvasp"
+alias gosbin="cd $HOME/sbins"
+alias godown="cd $HOME/Downloads"
+alias govas="cd $HOME/softwares/VASP"
+alias gotf="cd $HOME/anaconda/lib/python2.7/site-packages/tensorflow"
+alias gomod="cd /home/joonho/Downloads/modules-4.1.1/init"
+alias goex="cd $HOME/exe-py"
+export dcode="/shared/share_win/share_AI/Codes"
+
+# shared directory
+shared=/shared/share_win
+alias gosh="cd $shared"
+alias gosh1="cd $shared/share_Res1"
+alias gosh2="cd $shared/share_Res2"
+alias gow1r="cd $Res/Mofplus"
+alias gow="cd $shared/share_Res2/share_CO2_red"
+alias gow1="cd $shared/share_Res2/"
+alias gow2="cd $shared/share_Res2/share_MOF/MOF-growth"
+alias gow3="cd $shared/share_Res2/share_QE"
+alias gok="cd $shared/share_KCRC"
+alias gon="cd $shared/share_NRF"
+alias goshai="cd $shared/share_AI/"
+#alias goc="cd /shared/share_win/share_CO2_red"
+
+alias source_a="source $HOME/scripts/alias.sh"
+alias vi_a="vi $HOME/scripts/alias.sh"
+#share_ni=$shared/share_CO2_red
+
+# QChem
+alias gtot="grep 'total energy'"
+alias gTot="grep 'Total energy'"
+alias gfin="grep 'Final energy'"
+alias gfina="grep 'Final energy' *.out"
+alias gret="grep Thank"
+alias grett="grep Thank *.out"
+alias greo="grep 'Optimization Cycle'"
+alias gTOT="grep 'TOTAL ENERGY'"
+alias goqc="cd $QC"
+alias goqch="cd $QCHOME"
+alias goqce="cd $QC_EXT_LIBS"
+
+alias gosand="cd $HOME/sandbox"
+alias goc="cd $HOME/sandbox/pycommon"
+alias gomod="cd $HOME/privatemodules"
+alias gogro="cd $GHOME"
+
+# python
+export pyhome="/usr/lib/python2.7/dist-packages"
+export piphome="/usr/local/lib/python2.7/dist-packages"
+export pyana="/home/joonho/anaconda/lib/python2.7/site-packages"
+
+# ASE
+alias ag="ase gui"
+
+# xmgrace
+export gracehome="$HOME/env"
+
+
