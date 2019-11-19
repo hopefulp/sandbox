3a6e29c2c7df060610567ef039a1ee2f7d61282b
Joonho Park <hopefulp@gmail.com>
2019-11-19 15:18:35 +0900

    wrong MD

--- pycommon/file_cli.sh
+++ pycommon/file_cli.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
+### sandbox directories on each server, N.B. in sub shell, ENV is not inherited
+#hostname=$(hostname)
+#if [ $hostname -eq "chi" ]; then
+#    SB=/home/joonho/sandbox_gl
+#elif [ $hostname -eq "login" ]; then
+#    SB=/gpfs/home/joonho/sandboxg
+#else
+#    SB=/home01/x1813a01/sandbox
+#fi
+
 server_dir="x1813a01@nurion.ksc.re.kr:/scratch/x1813a01"
-SBB=/gpfs/home/joonho/sandboxg/pycommon/
-### job 0        1      2     3    4  for CASE
-job=( "scp_kisti" "run_python" )
 
 if [ $# -eq 0 ]; then
-    echo "Usage:: $0 job_index \$2[next option]"
+    echo "Usage:: $0 [python_dir] job_index \$2[next option]"
     echo "job index: [0]scp_kisti "
     exit
 fi
+
+dir_name="pycommon"
+if [[ $1 =~ [a-z] ]]; then
+    dir=${1:-$dir_name}
+    shift
+else
+    dir=$dir_name
+fi
+
+SBB=${SB}${dir}
+echo $SBB
+
+### job 0        1      2     3    4  for CASE
+job=( "scp_kisti" "run_python" )
+
 
 j=${job[$1]}
 f=$2
@@ -20,7 +42,7 @@ case $j in
         echo "scp $f $server_dir"
         ;;
     "run_python")
-        echo "python $SBB/$2 $3 $4"
+        echo "python $SBB/$2 ${@:3:3}"
         ;;
     ### default::
     *)
