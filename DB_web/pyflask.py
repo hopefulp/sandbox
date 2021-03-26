9a8d21a76da456cc3e7a11e78dbb69826f763b6c
Joonho Park <hopefulp@gmail.com>
2021-03-26 15:48:21 +0900

    more

--- DB_web/pyflask.py
+++ DB_web/pyflask.py
@@ -4,10 +4,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+#@app.rout('/')
+@app.route('/user/<user_name>/<int:user_id>')
 
-def home():
-    return 'Hello, World!'
+#def home():
+#    return 'Hello, World!'
+def user(user_name, user_id):
+    return f'Hello, {user_name}({user_id})!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
