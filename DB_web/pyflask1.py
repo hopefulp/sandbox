186b5779c958f839871064403f4aa09052464bad
Joonho Park <hopefulp@gmail.com>
2021-04-01 16:22:20 +0900

    more

--- DB_web/pyflask1.py
+++ DB_web/pyflask1.py
@@ -5,12 +5,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+#@app.route('/user/<user_name>/<int:user_id>')
 
 def home():
     return 'Hello, World!'
+#def user(user_name, user_id):
+#    return f'Hello, {user_name}({user_id})!'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='143.248.249.73', port=4999)
 
 
