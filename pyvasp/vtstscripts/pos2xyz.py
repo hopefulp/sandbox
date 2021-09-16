7f3513961b87fb3f83d1681d9695ed4fcf47e1d0
Joonho Park <hopefulp@gmail.com>
2021-09-16 19:02:15 +0900

    more

--- pyvasp/vtstscripts/pos2xyz.py
+++ pyvasp/vtstscripts/pos2xyz.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 import aselite
 from sys import argv
+import re
 
 if '-h' in argv or len(argv) != 2:
     print('usage: pos2xyz.py POSCAR')
     print()
     exit(1)
 
-atoms = aselite.read_vasp(argv[1])
-aselite.write_xyz('%s.xyz' % argv[1], atoms)
+fname = argv[1]
+atoms = aselite.read_vasp(fname)
+if re.match('POSCAR.', fname) or re.match('CONTCAR.', fname):
+    fnamelist=fname.split('.')
+    outf=fnamelist[1]+'.xyz'
+else:
+    outf=fname+'.xyz'
+print(f"output fname = {outf}")
+aselite.write_xyz('%s' % outf, atoms)
