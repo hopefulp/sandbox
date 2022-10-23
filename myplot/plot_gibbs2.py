1cdf0f2552092d6da6839e5c5748849dbd0da618
joonho <hopefulp@gmail.com>
2022-10-23 19:07:26 +0900

    plot gibbs

---
+++ myplot/plot_gibbs2.py
@@ -0,0 +1,94 @@
+#!/home/joonho/anaconda3/bin/python
+import pandas as pd
+import argparse
+import re
+import os
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import csv
+import sys
+import numpy as np
+
+
+def plot_gibbs(fname):
+
+    df = pd.read_csv(fname)
+    total_rows=len(df.axes[0])#number of row
+    total_cols=len(df.axes[1])#number of column
+
+    rowindexes=[]
+    colindexes=[]
+
+    for i in range(total_rows):
+        rowindexes.append(df.loc[i][0])
+
+#    print(rowindexes) <--- Check Complete, Successful!
+
+    for i in range (0,total_rows):
+        for j in range (1,total_cols):
+            colindexes.append(df.loc[i][j])
+
+    print(rowindexes,colindexes,total_cols)
+
+    '''
+    xs=[]
+    ene=[]
+    with open(fname, 'r') as f:
+
+#       lines=f.readlines()
+#       for line in lines:
+#           en = float(line.strip())
+#           ene.append(en)
+        reader = csv.DictReader(f)
+        for row in reader:
+            xs.append()
+            ene.append()
+    '''
+
+
+    ### matplotlib 
+    fig = plt.figure()
+    ax = fig.add_subplot()
+
+    plt.ylabel('E (eV)',rotation=0, labelpad=18)
+    varnum=total_rows
+
+
+
+
+
+
+#data line
+    for i in range(varnum):
+        for j in range((total_cols-1)*i,(total_cols-1)*i+(total_cols-1)):
+            plt.plot([i*2 ,i*2+1], [colindexes[j],colindexes[j]],color='k')
+#connection line
+    for i in range(varnum-1):#0~9
+        for j in range(total_cols-1):#4
+            plt.plot([i*2+1,i*2+2],[colindexes[(total_cols-1)*i+j],colindexes[((total_cols-1)*(i+1))+j]],color='k',linestyle='--',linewidth=0.5)
+#erase all xticks
+#    plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+#adding xticks
+    ax.plot()
+
+    xx=list(np.arange(0.5,varnum*2,2))
+    ax.set_xticks(xx)
+    ax.set_xticklabels(rowindexes, fontsize=8)
+        
+        
+
+
+
+
+    plt.show()
+    return 0
+
+def main():
+    parser = argparse.ArgumentParser(description='To get rid of abnormal DOS at start energy')
+    parser.add_argument('inf')
+    args = parser.parse_args()
+    
+    plot_gibbs(args.inf) 
+
+if __name__ == '__main__':
+    main()
