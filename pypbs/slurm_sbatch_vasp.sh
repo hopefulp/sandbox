0247c475f8276f31fddb427a3cb22e2c0620eabd
joonho <hopefulp@gmail.com>
2022-12-01 17:46:15 +0900

    pt slurm

--- pypbs/slurm_sbatch_vasp.sh
+++ pypbs/slurm_sbatch_vasp.sh
@@ -3,10 +3,10 @@
 ##SBATCH -p X3               # X2(16), X3(22), X4(4)  
 ##SBATCH -N 6               # total number of nodesmpi tasks requested
 ##SBATCH -n 64               # total number of mpi tasks requested
-##SBATCH --nodelist=n[053-068]
+###SBATCH --nodelist=n[053-068]
 
 ## HPC ENVIRONMENT
-. /etc/profile.d/SLURM.sh
+#. /etc/profile.d/SLURM.sh
 
 #mpiexec.hydra -np $SLURM_NTASKS  /TGM/Apps/VASP/bin/5.4.4/NORMAL/vasp_5.4.4_GRP7_NORMAL_20170903.x
 #mpirun -np $SLURM_NTASKS  /home/starnmj/bin/vasp_5.4.4_GRP7_NORMAL_20170903.x
