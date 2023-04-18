3864a6e0582c58028da0c591b3203b777a991f75
Joonho Park <hopefulp@gmail.com>
2023-04-18 22:49:58 +0900

    qdel

--- pypbs/qdel.sh
+++ pypbs/qdel.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+function cut_dot {
+    pre=`cut $1 -d. -f1`
+    echo $pre
+    }
+
+
 if [ $# -lt 2 ]; then
     echo "input two numbers: \$1 for start process \$2 for the last process"
     exit 1
 fi
 
-i=$1
-n=$(expr $2 + 1)
-
-
 list=()
-host=$(hostname)
 
-while [ $i -lt $n ]; do
-    echo $qdel $i
+for x in $@; do
+    if [[ $x =~ "." ]]; then
+        x=`echo $x | cut -d. -f1`
+    fi
+    list+=($x)
+done
+
+i=${list[0]}
+n=${list[1]}
+
+echo $i, $n
+
+while [ $i -le $n ]; do
+    echo qdel $i
     qdel $i
     i=$(expr $i + 1)
     done
