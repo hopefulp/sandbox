b806ca4eff0c2c42721d1999ebef4e9f27070f53
joonho <hopefulp@gmail.com>
2023-10-04 13:54:44 +0900

    slurm sleep

--- pypbs/slurm_sbatch_sleep.sh
+++ pypbs/slurm_sbatch_sleep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J sleep              # job name, get jobname from dirname
-#SBATCH -p X1               # X2(16), X3(22), X4(4)  
+#SBATCH -p X3               # X2(16), X3(22), X4(4)  
 #SBATCH -N 1              # total number of nodesmpi tasks requested
-#SBATCH -n 8               # total number of mpi tasks requested
-#SBATCH --nodelist=n029
+#SBATCH -n 20               # total number of mpi tasks requested
+#SBATCH --nodelist=n068
 ## HPC ENVIRONMENT
 #. /etc/profile.d/SLURM.sh
 
