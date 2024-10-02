4b3f4a66846729c4c7df006440ebe6b698576302
Joonho Park <hopefulp@gmail.com>
2024-10-02 16:28:20 +0900

    tmp

--- pyaseoori/ase_fconvert.py
+++ pyaseoori/ase_fconvert.py
@@ -9,15 +9,18 @@ import sys, re
 ###        fname : format
 formats = { 'POSCAR': 'vasp'}
 
-def ase_convert(ifile, ofile, off, movement, amount_m):
+def ase_convert(ifile, ofile, iff, off, movement, amount_m):
     ### Read
-    atoms = read(ifile)
-
+    try:
+        atoms = read(ifile, format=iff)
+    ### if fails read file format
+    except:
+        print(f"try input file format such as -if [vasp|vasp-out|vasp-xdatcar|vasp-xml|...]")
     ###### Output file format
     ### 1. extension is fformat
     if off:
         outff = off
-    elif re.match('\.', ofile):
+    elif re.search('\.', ofile):
         outff = f_ext(ofile)     # output file format
     ### outfname has format in formats
     elif ofile in formats.keys():
@@ -34,8 +37,11 @@ def ase_convert(ifile, ofile, off, movement, amount_m):
         elif movement=='r':
             pass        # not ready
 
-
-    write(ofile, atoms, format=outff)
+    try: 
+        write(ofile, atoms, format=outff)
+        print(f"{ofile} is written")
+    except: 
+        print(f"Error in writing {ofile}")
 
     return 0
 
@@ -43,13 +49,14 @@ def main():
     parser = argparse.ArgumentParser(description="read extxyz and write POSCAR  ")
     parser.add_argument('inf', help="input file")
     parser.add_argument('outf',  help="output file")
+    parser.add_argument('-if','--inpff',  help="input file format")
     parser.add_argument('-of','--outff',  help="output file format")
     manipulate = parser.add_argument_group()
     manipulate.add_argument('-m', '--move', choices={'t','r'},  help="move molecule by translate|rotate")
     manipulate.add_argument('-ma', '--amount_move', default=0.5, help="amount of movement: translate w.r.t. cell size")
     args = parser.parse_args()
 
-    ase_convert(args.inf, args.outf, args.outff, args.move, args.amount_move )
+    ase_convert(args.inf, args.outf, args.inpff, args.outff, args.move, args.amount_move )
 
 if __name__ == "__main__":
     main()
